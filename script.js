da8741985ca98c5c25286a013343774aa06c4710
tpklinh8408-eng <tpklinh8408@gmail.com>
2026-05-22 00:04:30 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,21 @@
-const wrapper = document.querySelector(".wrapper");
-const question = document.querySelector(".question");
-const gif = document.querySelector(".gif");
-const yesBtn = document.querySelector(".yes-btn");
-const noBtn = document.querySelector(".no-btn");
-const questionPrompt = document.querySelector(".question-prompt");
-
-// Khi chúng nó bấm vào nút TẤT NHIÊN RỒI
-yesBtn.addEventListener("click", () => {
-    question.innerHTML = "Ok cưng, sáng suốt quá. Ta cho ngươi lui 🥰";
-    // Ẩn cả hai nút và dòng nhắc nhở đi cho đẹp màn hình
-    yesBtn.style.display = "none";
-    noBtn.style.display = "none";
-    if(questionPrompt) questionPrompt.style.display = "none";
-});
-
-// Khi chúng nó di chuột định bấm vào nút Không mđ (Nút này sẽ tự chạy trốn)
-noBtn.addEventListener("mouseover", () => {
-    const maxX = window.innerWidth - noBtn.offsetWidth;
-    const maxY = window.innerHeight - noBtn.offsetHeight;
-
-    const randomX = Math.floor(Math.random() * maxX);
-    const randomY = Math.floor(Math.random() * maxY);
-
-    noBtn.style.position = "absolute";
-    noBtn.style.left = randomX + "px";
-    noBtn.style.top = randomY + "px";
-});
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lưu bút</title>
+    <link rel="stylesheet" href="style.css"/>
+</head>
+<body>
+    <div class="wrapper">
+        <h2 class="question">T LÀ NGƯỜI BẠN CUTE VÀ HIỀN LÀNH NHẤT M TỪNG GẶP ĐÚNG KHUM 👈👉</h2>
+        <h4 class="question-prompt">NẾU M THOÁT THÌ M ĂN ĐÒN</h4>
+        <img id="imageDiv" src="https://media.giphy.com/media/PcCh9x9Pz5d2CriuMQ/giphy.gif" alt="">
+        <div class="btn-group">
+            <button class="yes-btn">TẤT NHIÊN RỒI 😻</button>
+            <button class="no-btn">Không mày 😡</button>
+        </div>
+    </div>
+    <script src="script.js"></script>
+</body>
+</html>
